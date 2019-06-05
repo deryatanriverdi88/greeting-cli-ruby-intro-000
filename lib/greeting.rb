12dc755bace8f2456aa5daf107902395a44caf0f
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-06-05 05:23:40 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,11 @@
 # code the #greeting method here!
 
-def greeting name
+def greeting(name)
   puts "Hello #{name.chomp.strip}. It's nice to meet you."
 end
+
+def greeting(name)
+  puts "Hello #{name}             .It's nice to meet you"
+end
+
+
